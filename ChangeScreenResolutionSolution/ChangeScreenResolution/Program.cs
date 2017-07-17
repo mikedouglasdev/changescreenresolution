cd2a41bbefccaaab07dc93e6b51ebb2f86008c2f
Mike Douglas <mikedouglas@deliveron.com>
2017-07-16 23:22:46 -0500

    changes for chocolatey

--- ChangeScreenResolutionSolution/ChangeScreenResolution/Program.cs
+++ ChangeScreenResolutionSolution/ChangeScreenResolution/Program.cs
@@ -13,6 +13,13 @@ namespace ChangeScreenResolution
 {
     class Program
     {
+        /// <summary>
+        /// Primary entry point of console application.  This application should be called with width and height arguments on computer startup
+        /// In Windows 10, use Start > Run > shell:common startup to open folder (C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup)
+        /// create shortcut for app and copy to folder
+        /// add arguments
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
 
@@ -23,9 +30,10 @@ namespace ChangeScreenResolution
 
             try
             {
+                string message;
                 int width = 1920;
                 int height = 1080;
-                int retries = 3;
+                int retries = 10;
 
                 if (args.Length > 0)
                     int.TryParse(args[0], out width);
@@ -39,7 +47,10 @@ namespace ChangeScreenResolution
                 string emailbody = "";
                 DisplaySettings set = DisplayManager.GetCurrentSettings();
 
-                emailbody = "Original resolution = " + set.Width.ToString() + " x " + set.Height.ToString() + "<br />";
+                message = "Original resolution = " + set.Width.ToString() + " x " + set.Height.ToString() + "<br />";
+                LogMessage(message);
+                emailbody = message;
+
                 List<DisplaySettings> displaySettings = new List<DisplaySettings>(DisplayManager.GetModesEnumerator().ToIEnumerable<DisplaySettings>());
 
                 emailbody = emailbody + "Available Modes: <br />";
@@ -54,21 +65,34 @@ namespace ChangeScreenResolution
                 {
                     try
                     {
+                        LogMessage("Attempting to change display settings to " + width + " x " + height + ".  Retries remaining " + (retries - 1).ToString());
                         after = ChangeDisplay(width, height, displaySettings);
                         break; // exit while loop
                     }
-                    catch
+                    catch(Exception ex)
                     {
-                        if (--retries == 0) throw;
-                        else Thread.Sleep(1000);
+
+                        if (--retries == 0)
+                        {
+                            LogMessage("Change Screen Resolution retry failed. No more attempts.");
+                            throw;
+                        }
+                        else
+                        {
+                            LogMessage("Change Screen Resolution retry failed. Retrying in 1 second.");
+                            Thread.Sleep(1000);
+                        }
                     }
                 }
 
                 emailbody = emailbody + "<br />" + "New resolution = " + after.Width.ToString() + " x " + after.Height.ToString();
 
+                message = "ChangeScreenResolution - resolution set on " + Environment.MachineName;
+                LogMessage(message);
+
                 if (emailEnabled)
                 {
-                    SendEmail(emailTo, "ChangeScreenResolution - resolution set on " + Environment.MachineName, emailbody);
+                    SendEmail(emailTo, message, emailbody);
                 }
             }
             catch (Exception ex)
@@ -76,7 +100,9 @@ namespace ChangeScreenResolution
                 if(emailEnabled)
                 {
                     string emailBody = ex.ToString();
-                    SendEmail(emailTo, "ChangeScreenResolution - exception thrown on " +Environment.MachineName, emailBody);
+                    string title = "ChangeScreenResolution - exception thrown on " + Environment.MachineName;
+                    LogMessage(title + " - " + emailBody);
+                    SendEmail(emailTo, title, emailBody);
 
                 }
                 Console.WriteLine(ex.ToString());
@@ -84,6 +110,13 @@ namespace ChangeScreenResolution
             }
         }
 
+        /// <summary>
+        /// Method that wraps win 32 api call to change the display settings
+        /// </summary>
+        /// <param name="width">number of pixels wide such as 1920</param>
+        /// <param name="height">number of pixels tall such as 1080</param>
+        /// <param name="displaySettings">List of available screen modes supported</param>
+        /// <returns></returns>
         private static DisplaySettings ChangeDisplay(int width, int height, List<DisplaySettings> displaySettings)
         {
             var hd = displaySettings.FirstOrDefault(d => d.Height == height && d.Width == width);
@@ -94,7 +127,13 @@ namespace ChangeScreenResolution
             return after;
         }
 
-        static void SendEmail(string email, string subject, string body)
+        /// <summary>
+        /// Can send email notification using SendGrid
+        /// </summary>
+        /// <param name="email">email recipient(s)</param>
+        /// <param name="subject">title of the email</param>
+        /// <param name="body">details of the email</param>
+        private static void SendEmail(string email, string subject, string body)
         {
             if (string.IsNullOrEmpty(email))
                 return;
@@ -115,8 +154,29 @@ namespace ChangeScreenResolution
 
             transportWeb.Deliver(message);
         }
+
+        /// <summary>
+        /// Can Log Message to a local file.  Can be toggled on or off from config file.
+        /// </summary>
+        /// <param name="message">Details of what will be logged</param>
+        private static void LogMessage(string message)
+        {
+            if(ConfigurationManager.AppSettings["LoggingEnabled"] == null || ConfigurationManager.AppSettings["LoggingEnabled"].ToLower() != "true")
+            {
+                return;
+            }
+            string filename = DateTime.Today.ToString("yyyy-MM-dd") + "_log.txt";
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true))
+            {
+                file.WriteLine(DateTime.Now.ToString("hh:mm:ss") + " - " + message);
+            }
+        }
     }
 
+    /// <summary>
+    /// Extension method that returns list
+    /// </summary>
     public static class EnumeratorExtensions
     {
         public static IEnumerable<T> ToIEnumerable<T>(this IEnumerator<T> enumerator)
